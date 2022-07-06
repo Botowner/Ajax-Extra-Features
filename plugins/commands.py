5d001aa6ae2a28cef0342844faf657b82f215fb2
Zendaya <100590136+Botowner@users.noreply.github.com>
2022-07-06 10:43:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,7 +56,7 @@ async def start(client, message):
         await m.delete()        
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=START_MESSAGE.format(user=message.from_user.mention, bot=temp.B_LINK),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
